948c83db5943144dcf0073b9db26de34883a4549
it00ls <888888@it00ls.com>
2015-10-27 13:30:41 +0800

    闪屏页面完成

---
+++ app/src/main/java/com/it00ls/mobilesafe/activity/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.it00ls.mobilesafe.activity;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.it00ls.mobilesafe.R;
+
+
+/**
+ * @author IT00LS
+ * @version x.xx 2015/10/27 12:25
+ */
+public class SplashActivity extends Activity {
+
+    private TextView tv_version;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        tv_version = (TextView) findViewById(R.id.tv_version);
+        tv_version.setText("版本号:" + getVersionName());
+    }
+
+    public String getVersionName() {
+        PackageManager packageManager = getPackageManager();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
